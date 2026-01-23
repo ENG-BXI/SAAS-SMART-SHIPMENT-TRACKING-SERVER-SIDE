423402399d230fe23d56978235c0cab1592616ec
ENG_BXI <eng.0.bxi@gmail.com>
2026-01-23 07:49:00 +0300

    merge company and add subscription module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './V1/company/company.module';
 import { ConfigModule } from '@nestjs/config';
+import { SubscriptionModule } from './V1/subscription/subscription.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CompanyModule],
+  imports: [ConfigModule.forRoot(), CompanyModule, SubscriptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
