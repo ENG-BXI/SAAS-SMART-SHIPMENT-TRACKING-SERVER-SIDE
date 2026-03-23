3e54e5402a264fdf7983e43f59f19a98eaa88739
ENG_BXI <eng.0.bxi@gmail.com>
2026-03-23 05:41:50 +0300

    work in user module and add client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CompanyModule } from './V1/company/company.module';
 import { ConfigModule } from '@nestjs/config';
 import { SubscriptionModule } from './V1/subscription/subscription.module';
 import { ClientModule } from './client/client.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CompanyModule, SubscriptionModule, ClientModule],
+  imports: [ConfigModule.forRoot(), CompanyModule, SubscriptionModule, ClientModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
