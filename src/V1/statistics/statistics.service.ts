8dcbd3540133508c59b95ed38ef7b6691ef39a08
ENG_BXI <eng.0.bxi@gmail.com>
2026-05-19 19:18:13 +0300

    feat: implement statistics services and add audit timestamps to user model

--- src/V1/statistics/statistics.service.ts
+++ src/V1/statistics/statistics.service.ts
@@ -60,5 +60,90 @@ export class StatisticsService {
     };
   }
 
-  async getAdminStatistics() {}
+  /*
+  numberOfCompanies
+  numberOfVisited
+  numberOfNotes
+  numberOfWillSubscriptionFinish
+  numberOfPausedCompanies
+
+  LineChart => numberOfCompanyByMonth
+  {
+    month: string,
+    count: number
+  }
+   */
+  async getAdminStatistics() {
+    const year = new Date().getFullYear();
+    const [
+      numberOfCompanies,
+      numberOfNotes,
+      numberOfWillSubscriptionFinish,
+      numberOfPausedCompanies,
+      CompanyByMonth,
+    ] = await Promise.all([
+      this.prisma.company.count(),
+      // TODO : numberOfVisited
+      this.prisma.note.count(),
+      this.prisma.company.count({
+        where: {
+          subscription: {
+            endDate: {
+              lt: new Date(),
+            },
+          },
+        },
+      }),
+      this.prisma.company.count({
+        where: {
+          subscription: {
+            status: 'inactive',
+          },
+        },
+      }),
+      this.prisma.company.findMany({
+        select: {
+          createdAt: true,
+        },
+        where: {
+          createdAt: {
+            gte: new Date(year, 0, 1),
+            lte: new Date(year, 11, 31),
+          },
+        },
+      }),
+    ]);
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    const returnCompaniesByMonth = Array.from({ length: 12 }, (_, index) => ({
+      month: monthNames[index],
+      count: 0,
+    }));
+
+    CompanyByMonth.forEach((company) => {
+      const monthIndex = new Date(company.createdAt).getMonth();
+      returnCompaniesByMonth[monthIndex].count++;
+    });
+
+    return {
+      numberOfCompanies,
+      numberOfNotes,
+      numberOfWillSubscriptionFinish,
+      numberOfPausedCompanies,
+      numberOfCompanyByMonth: returnCompaniesByMonth,
+    };
+  }
 }
