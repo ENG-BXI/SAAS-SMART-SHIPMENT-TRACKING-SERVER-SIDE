0c6c65fd206c1ce4e1084118ffdbc8e16d3cf2f2
ENG_BXI <eng.0.bxi@gmail.com>
2026-06-11 23:59:32 +0300

    feat: Add AuthRepository with login method for user authentication

---
+++ src/V1/auth/auth.repository.ts
@@ -0,0 +1,32 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+
+export class AuthRepository {
+  constructor(private prisma: PrismaService) {}
+  async login(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        isAdmin: true,
+        isDriver: true,
+        isEmployee: true,
+        isManager: true,
+        password: true,
+        id: true,
+        email: true,
+        userName: true,
+        companyId: true,
+        company: {
+          select: {
+            subscription: {
+              select: {
+                status: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+}
